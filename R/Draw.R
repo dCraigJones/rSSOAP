5a8e983b627459374fc01c2e7ee4279d7dd60030
Craig Jones <jones.dcraig@gmail.com>
2020-05-21 01:23:45 -0400

    added plots to summarize I&I model

--- R/Draw.R
+++ R/Draw.R
@@ -166,3 +166,111 @@ draw_model_qaqc <- function(df) {
     facet_grid(measure ~ ., scales="free_y") +
     labs(x="", y="flow (gpd)")
 }
+
+draw_qq <- function(field, model, units="psi", error=0.10, minVal=2) {
+    fx <- quantile(field, probs=(2:99/100))
+    my <- quantile(model, probs=(2:99/100))
+
+    qqplot(fx, my
+           , xlab=paste0("field (", units,")")
+           , ylab=paste0("model (", units,")")
+           , pch=4
+    )
+    x <- seq(min(0,10*min(fx, my)),10*max(fx,my),length.out=100)
+
+    yy1 <- error*x+x
+    yy2 <- -error*x+x
+
+    zz1 <- x+minVal
+    zz2 <- x-minVal
+
+    df <- cbind(yy1, yy2, zz1, zz2)
+    yy1 <- apply(df, 1, min)
+    yy2 <- apply(df, 1, max)
+
+    polygon(
+      c(x,rev(x))
+      , c(yy1, rev(yy2))
+      , col=rgb(0.25,0.25,0.25,0.25)
+      , border=NA
+    )
+
+    abline(a=0, b=1, lwd=1, lty=2, col="blue")
+
+    box(lwd=2)
+
+    res <- as.numeric(abs(my-fx))
+    mpe <- as.numeric(res/fx)
+
+    pts_failed <- sum(res>minVal & mpe>error)
+
+    subtitle <- paste0(round(error*100,0), "% error or ", minVal ," ", units, " (", 100-pts_failed, "% pass)")
+
+    title(sub=subtitle, adj=1, cex.sub=0.75)
+}
+
+draw_diurnal <- function(diurnal) {
+  plot(diurnal
+   , axes=F
+   , xlab=""
+   , ylab="base wastewater flow (gpm)"
+   , type="l"
+   , lwd=2
+   )
+  axis(2)
+  axis(1, at=1:7, labels=c("S", "M", "T", "W", "T", "F", "S"))
+  box(lwd=2)
+
+}
+
+draw_gwi <- function(datetime, gwi) {
+  datetime <- hf$datetime
+  gwi <- hf$gwi
+
+  tmp <- data.frame(datetime, gwi)
+
+  use <-   tmp %>% mutate(wk=week(datetime))
+
+  tmp <- tmp %>%
+    mutate(wk=week(datetime)) %>%
+    group_by(wk) %>%
+    summarize(gwi_wk=mean(gwi, na.rm=TRUE)) %>%
+    right_join(use, by="wk")
+
+  plot(datetime
+   , rollapply(tmp$gwi_wk, 14*24, mean, na.rm=TRUE, fill=NA)
+   , type="l"
+   , ylab="ground water infiltration (gpm)"
+   , xlab=""
+   , lwd=2
+  )
+
+  box(lwd=2)
+
+}
+
+draw_hydrograph <- function(uh) {
+  plot(0:24, uh
+    , xlab="time (hrs)"
+    , ylab="rainfall derived inflow (gpm)"
+    , type="l"
+    , lwd=2
+    , axes=FALSE
+  )
+  axis(2)
+  axis(1, at=seq(0,24,6))
+  box(lwd=2)
+
+}
+
+
+draw_ii <- function(datetime, flow, gwi, model, diurnal, uh, STATION_NAME="") {
+    layout(matrix(c(1,1,2,2,3,4), nrow=3, byrow=TRUE))
+    par(mar=c(1,5,2,2))
+    draw_diurnal(diurnal)
+    title(STATION_NAME, adj=0)
+    draw_gwi(datetime, gwi)
+    par(mar=c(5,5,2,2))
+    draw_hydrograph(uh)
+    draw_qq(flow, model, "gpm", 0.1, 25)
+}
