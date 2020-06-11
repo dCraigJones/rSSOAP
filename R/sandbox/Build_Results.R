345058e6272408a6744e7974d65839d1f1b536ed
Craig Jones <jones.dcraig@gmail.com>
2020-06-11 10:49:34 -0400

    minor formatting changes

--- R/sandbox/Build_Results.R
+++ R/sandbox/Build_Results.R
@@ -11,22 +11,23 @@ rain_ <- hourly_rain %>%
   #filter(station=="SOWEST")
 
 tmp <- hourly_flow %>%
-  filter(str_detect(location, "Car")) %>%
+  filter(str_detect(location, "Tow")) %>%
   mutate(date=date(datehour)) %>%
   filter(date>mdy("10/1/2018")) %>%
   filter(date<mdy("01/01/2020")) %>%
   left_join(rain_, by=c("datehour"="datetime"))
 
 
+
 date <- tmp$datehour
 flow <- tmp$flow
 rain <- tmp$rainfall_in
 
-rmarkdown::render("R/md/hourly_html.Rmd"
+rmarkdown::render("R/md/hourly_pdf.Rmd"
    , params = list(
           date = tmp$datehour
         , flow = tmp$flow
         , rain = tmp$rainfall_in
-        , gwi_pct=0.80
+        , gwi_pct=0.90
    )
 )
