6e240af17184d607094c6d965525016d2a7bc389
Craig Jones <jones.dcraig@gmail.com>
2026-04-09 23:33:46 -0400

    move hourly_html.Rmd to R/templates/ and rewire references

--- dev/sandbox/Build_Results.R
+++ dev/sandbox/Build_Results.R
@@ -35,7 +35,7 @@ rain %>%
   right_join(tmp, by=c("datetime"="datehour")) -> tmp
 
 
-rmarkdown::render("dev/templates/hourly_html.Rmd"
+rmarkdown::render("R/templates/hourly_html.Rmd"
    , params = list(
           date = tmp$datetime
         , flow = tmp$flow*1.15
@@ -61,7 +61,7 @@ rain %>%
 
 
 
-rmarkdown::render("dev/templates/hourly_html.Rmd"
+rmarkdown::render("R/templates/hourly_html.Rmd"
                   , params = list(
                     date = tmp$datetime
                     , flow = tmp$flow*1.2
@@ -84,7 +84,7 @@ rain %>%
   right_join(tmp, by=c("datetime"="datehour")) -> tmp
 
 
-rmarkdown::render("dev/templates/hourly_html.Rmd"
+rmarkdown::render("R/templates/hourly_html.Rmd"
                   , params = list(
                     date = tmp$datetime
                     , flow = tmp$flow
@@ -112,7 +112,7 @@ rain %>%
   right_join(tmp, by=c("datetime"="datehour")) -> tmp
 
 
-rmarkdown::render("dev/templates/hourly_html.Rmd"
+rmarkdown::render("R/templates/hourly_html.Rmd"
                   , params = list(
                     date = tmp$datetime
                     , flow = tmp$flow
